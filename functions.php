2d747312fa1196f3c712c7c1d237354400ed2c4d
Josh Reisner <josh@joshreisner.com>
2017-04-09 12:04:28 -0700

    Service sidebar

--- functions.php
+++ functions.php
@@ -33,5 +33,15 @@ add_action('widgets_init', function(){
 		'before_title'  => '<h3>',
 		'after_title'   => '</h3>',
     ));
+
+	register_sidebar( array(
+		'name'          => 'Service Sidebar',
+		'id'            => 'content-service',
+		'description'   => 'Additional sidebar that appears on the right of the Service section pages.',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h1 class="widget-title">',
+		'after_title'   => '</h1>',
+	) );
 	
 }, 11);
