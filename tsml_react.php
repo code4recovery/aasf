34974368f78e35728c0e923db7904e558545ec7f
Josh Reisner <1551689+joshreisner@users.noreply.github.com>
2021-07-02 07:57:50 -0700

    undoing my change

--- tsml_react.php
+++ tsml_react.php
@@ -6,23 +6,27 @@ and sends it to https://github.com/code4recovery/tsml-ui
 */
 
 add_shortcode('tsml_react', function() {
-    wp_enqueue_script('tsml_react', 'https://react.meetingguide.org/app.js');
-    wp_localize_script('tsml_react', 'tsml_react_config', [
-        'conference_providers' => [
-            'drydocksf.org' => 'Custom',
-            'MillValleyCabin.com' => 'Custom',
-            'sites.google.com' => 'Custom',
-            'tinyurl.com' => 'Custom',
-        ],
-        'strings' => [
-            'en' => [
+    $mapbox_key = 'pk.eyJ1IjoiYWFzZi1tcGFsIiwiYSI6ImNqaHl6NHY0NzByemszcHBkcHpib296OHkifQ.F7GfpF1jRosyQ0PGNUfDkA';
+    $conference_providers = [
+        'sites.google.com' => 'Custom',
+        'tinyurl.com' => 'Custom',
+        'MillValleyCabin.com' => 'Custom',
+        'drydocksf.org' => 'Custom',
+    ];
+    $host = 'https://react.meetingguide.org/app.js';
+    $url = 'https://airtable-json.aasfmarin.org';
+    wp_enqueue_script('tsml_react', $host);
+    wp_localize_script('tsml_react', 'tsml_react_config', array(
+        'timezone' => get_option('timezone_string'),
+        'conference_providers' => $conference_providers,
+        'show' => array(
+            'cityAsRegionFallback' => false,
+        ),
+        'strings' => array(
+            'en' => array(
                 'region' => 'City / Neighborhood',
-            ],
-        ]
-    ]);
-    return '<div id="tsml-ui"
-        data-mapbox="pk.eyJ1IjoiYWFzZi1tcGFsIiwiYSI6ImNqaHl6NHY0NzByemszcHBkcHpib296OHkifQ.F7GfpF1jRosyQ0PGNUfDkA"
-        data-src="https://airtable-json.aasfmarin.org"
-        data-timezone="' . get_option('timezone_string') . '"
-        ></div>';
+            ),
+        )
+    ));
+    return '<meetings src="' . $url . '" mapbox="' . $mapbox_key . '"/>';
 });
